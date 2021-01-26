e8ae947b3b113f022f5ad367a0d1ea21a813616b
Gizem Özdemir <gozdemir_92@outlook.com>
2021-01-26 20:17:28 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsDictionary
+{
+    class MyDictionary<KeyType, ValueType>
+    {
+        KeyType[] keys;
+        ValueType[] values;
+        KeyType[] tempKeys;
+        ValueType[] tempValues;
+
+        public MyDictionary()
+        {
+            keys = new KeyType[0];
+            values = new ValueType[0];
+        }
+        
+        public void Add (KeyType key, ValueType value)
+        {
+            tempKeys = keys;
+            keys = new KeyType[keys.Length + 1];
+            for (int i = 0; i < tempKeys.Length; i++) 
+            {
+                keys[i] = tempKeys[i];
+            }
+            keys[keys.Length - 1] = key;
+
+            tempValues = values;
+            values = new ValueType[values.Length + 1];
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+            values[values.Length - 1] = value;
+
+        }
+        public int valueCount
+        {
+            get { return values.Length; }
+
+        }
+        public int keyCount
+        {
+            get { return keys.Length; }
+        }
+
+        public KeyType[] keyArray
+        {
+            get { return keys; }
+        }
+        public ValueType[] valueArray
+        {
+            get { return values; }
+        }
+
+        public void Al(KeyType anahtar)
+        {
+            int sira = Array.IndexOf(keys, anahtar);
+            Console.WriteLine(values[sira]);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<Object, string> product = new MyDictionary<Object, string>();
+            product.Add(1, "Etek");
+            product.Add(3, "Kazak");
+            product.Add(4, "Pantolon");
+            product.Add(150, "Şapka");
+            product.Add(5, "Ayakkabı");
+            
+            Console.WriteLine("Product Name:" + product.keyCount);
+            Console.WriteLine("Product No:" + product.valueCount);
+
+
+
+            for (int i = 0; i < product.keyCount; i++)
+            {
+                Console.WriteLine(product.valueArray[i]);
+            }
+        }
+    }
+}
